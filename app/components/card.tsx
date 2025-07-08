a5a3ba61de8ac69975101c488d9ed956e0c6cddf
Ayush saini <ayushsaini7717@gmail.com>
2025-07-08 16:41:25 +0530

    fix/versionmismatch

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Tilt } from "react-tilt";
+import Tilt from 'react-parallax-tilt';
 
 type Card={
     title: string,
@@ -9,13 +9,22 @@ type Card={
 }
 const CardComp=({title,desc,icon}: Card)=>{
     return <>
-        <Tilt options={{max: 25,scale: 1.05,speed: 300}}>
-            <div className="flex flex-col w-full sm:w-[250px] md:w-[300px] lg:max-w-[18vw] border border-gray-300 rounded p-4 sm:p-5 bg-white shadow-sm">
+        <Tilt
+            glareEnable={true}
+            glareMaxOpacity={0.45}
+            scale={1.05}
+            transitionSpeed={250}
+            tiltMaxAngleX={20}
+            tiltMaxAngleY={20}
+            className="h-full w-full"
+            >
+            <div className="flex flex-col h-full w-full sm:w-[250px] md:w-[300px] lg:max-w-[18vw] border border-gray-300 rounded p-4 sm:p-5 bg-white shadow-sm">
                 <div>{icon}</div>
                 <div className="font-bold text-lg mt-2">{title}</div>
                 <div className="font-normal text-sm text-gray-500 mt-1">{desc}</div>
             </div>
         </Tilt>
+
     </>
   
 }
